778828b5a37f181b781073aab908d726067ccb39
Jacket777 <hxqsu37@sina.com>
2024-11-02 21:31:59 +0800

    坦克大战---netty简单版本

---
+++ test/nettychat/ClientFrame.java
@@ -0,0 +1,54 @@
+package nettychat;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class ClientFrame extends Frame {
+    private TextArea ta = new TextArea();
+    private TextField tf = new TextField();
+    private Client c = null;
+    public static final ClientFrame INSTANCE = new ClientFrame();
+
+    private ClientFrame(){
+        this.setSize(300,400);
+        this.setLocation(400,20);
+        this.add(ta,BorderLayout.CENTER);
+        this.add(tf,BorderLayout.SOUTH);
+        this.setTitle(" client chat");
+        tf.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               c.send(tf.getText());
+               ta.setText(ta.getText()+"\r\n"+tf.getText());
+                tf.setText("");
+            }
+        });
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                c.closeConnection();
+                System.exit(0);
+                super.windowClosing(e);
+            }
+        });
+    }
+
+    public void connectToServer() throws Exception {
+        c = new Client();
+        c.connect();
+    }
+
+    public static void main(String[] args) throws Exception {
+        ClientFrame cf = ClientFrame.INSTANCE;
+        cf.setVisible(true);
+        cf.connectToServer();
+    }
+
+    public void updateText(String str) {
+        ta.setText(ta.getText()+str+"\r\n");
+    }
+}
---
+++ test/nettychat/Server.java
@@ -0,0 +1,98 @@
+package nettychat;
+
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.CharsetUtil;
+import io.netty.util.ReferenceCountUtil;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+public class Server {
+    public static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    public static void main(String[] args) throws Exception{
+        //负责处理事件
+        EventLoopGroup bossGroup = new NioEventLoopGroup(2);
+        EventLoopGroup workGroup = new NioEventLoopGroup(4);
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(bossGroup, workGroup);//第一个参数的位置是boss的位置
+        //异步全双工
+        b.channel(NioServerSocketChannel.class);
+        b.childHandler(new MyChildServerInitializer());
+        ChannelFuture future = b.bind(8888).sync();
+        future.channel().closeFuture().sync();
+        bossGroup.shutdownGracefully();
+        workGroup.shutdownGracefully();
+    }
+
+}
+
+class MyChildServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        System.out.println(" a client connected");
+        //处理连接好了连接
+        socketChannel.pipeline().addLast(new MyChildNettyHandler());
+    }
+}
+
+class MyChildNettyHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        Server.clients.add(ctx.channel());//存管道
+    }
+
+    /**
+     * 接收数据
+     *
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf2 = (ByteBuf)msg;
+        System.out.println(buf2.toString());
+//        System.out.println(buf.toString(CharsetUtil.UTF_8));
+//      // buf.release();
+//        ctx.writeAndFlush(msg);
+        ByteBuf buf = null;
+        try {
+            buf = (ByteBuf) msg;
+            byte[] bytes = new byte[buf.readableBytes()];
+            buf.getBytes(buf.readerIndex(), bytes);
+            String str = new String(bytes);
+            System.out.println("client send " + str);
+            if (str.equals("__bye__")) {
+                System.out.println("client ready to quit");
+                Server.clients.remove(ctx.channel());
+                ctx.close();
+                System.out.println(Server.clients.size());
+            } else {
+              Server.clients.writeAndFlush(msg);//会自动对buf释放
+            }
+            //System.out.println(str);
+            //System.out.println(buf.refCnt());//拿到buf被多少个对象引用
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        finally {
+////            if (buf != null) {
+////                ReferenceCountUtil.release(buf);
+////            }
+//        }
+        //Server.clients.writeAndFlush(msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        Server.clients.remove(ctx.channel());
+        ctx.close();
+    }
+}
